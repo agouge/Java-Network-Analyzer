5076a17627195815d82ad515cf7b7e058c84595b
Adam Gouge <adam.gouge@ec-nantes.fr>
2013-03-26 15:37:32 +0100

    Add test for Strahler algorithm.

--- src/test/java/com/graphhopper/sna/centrality/DFSTest.java
+++ src/test/java/com/graphhopper/sna/centrality/DFSTest.java
@@ -1,17 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * GraphHopper-SNA implements a collection of social network analysis
+ * algorithms. It is based on the <a
+ * href="http://graphhopper.com/">GraphHopper</a> library.
+ *
+ * GraphHopper-SNA is distributed under the GPL 3 license. It is produced by the
+ * "Atelier SIG" team of the <a href="http://www.irstv.fr">IRSTV Institute</a>,
+ * CNRS FR 2488.
+ *
+ * Copyright 2012 IRSTV (CNRS FR 2488)
+ *
+ * GraphHopper-SNA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * GraphHopper-SNA is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * GraphHopper-SNA. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.graphhopper.sna.centrality;
 
-import com.graphhopper.sna.data.DFSInfo;
 import com.graphhopper.storage.Graph;
-import gnu.trove.iterator.TIntIterator;
-import gnu.trove.set.hash.TIntHashSet;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.Test;
+import com.graphhopper.storage.GraphStorage;
+import com.graphhopper.storage.RAMDirectory;
 
 /**
  * Tests the Depth First Search (DFS).
@@ -20,35 +35,47 @@ import org.junit.Test;
  */
 public class DFSTest {
 
-    /**
-     * Tests the {@link Graphs#weightedDirected()} graph.
-     *
-     * @throws FileNotFoundException
-     */
-    @Test
-    public void test2DGraph() throws FileNotFoundException {
-
-        Graph graph = Graphs.weightedDirected();
-        Map<Integer, DFSInfo> nodeMap = init(graph);
-
-        DFS dfs = new DFS(graph, nodeMap);
-        dfs.calculate();
-    }
+    private static final double ONE = 1.0;
 
     /**
-     * Initializes the node info map.
+     * Prepare a tree with root node 1.
      *
-     * @param graph The input graph.
-     *
-     * @return The newly initialized node info map.
+     * @return A tree with root node 1.
      */
-    private Map<Integer, DFSInfo> init(Graph graph) {
-        Map<Integer, DFSInfo> nodeMap = new HashMap<Integer, DFSInfo>();
-        TIntHashSet nodeSet = GeneralizedGraphAnalyzer.nodeSet(graph);
-        TIntIterator it = nodeSet.iterator();
-        while (it.hasNext()) {
-            nodeMap.put(it.next(), new DFSInfo());
-        }
-        return nodeMap;
+    protected Graph prepareTree() {
+
+        GraphStorage graph = new GraphStorage(new RAMDirectory());
+        graph.createNew(10);
+
+        graph.edge(1, 2, ONE, false);
+        graph.edge(1, 3, ONE, false);
+
+        graph.edge(3, 4, ONE, false);
+        graph.edge(3, 12, ONE, false);
+
+        graph.edge(4, 5, ONE, false);
+        graph.edge(4, 8, ONE, false);
+
+        graph.edge(5, 6, ONE, false);
+        graph.edge(5, 7, ONE, false);
+
+        graph.edge(8, 9, ONE, false);
+        graph.edge(8, 10, ONE, false);
+        graph.edge(8, 11, ONE, false);
+
+        graph.edge(12, 13, ONE, false);
+
+        graph.edge(13, 14, ONE, false);
+        graph.edge(13, 16, ONE, false);
+
+        graph.edge(14, 15, ONE, false);
+
+        graph.edge(16, 17, ONE, false);
+        graph.edge(16, 18, ONE, false);
+
+        graph.edge(18, 19, ONE, false);
+        graph.edge(18, 20, ONE, false);
+
+        return graph;
     }
 }
---
+++ src/test/java/com/graphhopper/sna/centrality/StrahlerTest.java
@@ -0,0 +1,101 @@
+/**
+ * GraphHopper-SNA implements a collection of social network analysis
+ * algorithms. It is based on the <a
+ * href="http://graphhopper.com/">GraphHopper</a> library.
+ *
+ * GraphHopper-SNA is distributed under the GPL 3 license. It is produced by the
+ * "Atelier SIG" team of the <a href="http://www.irstv.fr">IRSTV Institute</a>,
+ * CNRS FR 2488.
+ *
+ * Copyright 2012 IRSTV (CNRS FR 2488)
+ *
+ * GraphHopper-SNA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * GraphHopper-SNA is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * GraphHopper-SNA. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.graphhopper.sna.centrality;
+
+import com.graphhopper.sna.data.StrahlerInfo;
+import com.graphhopper.storage.Graph;
+import gnu.trove.iterator.TIntIterator;
+import gnu.trove.set.hash.TIntHashSet;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests calculating the Strahler numbers of the nodes in a tree.
+ *
+ * @author Adam Gouge
+ */
+public class StrahlerTest extends DFSTest {
+
+    /**
+     * Tests the {@link DFSTest#prepareTree()} tree.
+     *
+     * @throws NoSuchMethodException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    @Test
+    public void testTree() throws NoSuchMethodException, InstantiationException,
+            IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException {
+
+        Graph graph = prepareTree();
+        Map<Integer, StrahlerInfo> nodeMap = init(graph);
+        new DFSForStrahler(graph, nodeMap, 1).calculate();
+
+        assertEquals(nodeMap.get(1).getStrahlerNumber(), 3);
+        assertEquals(nodeMap.get(2).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(3).getStrahlerNumber(), 3);
+        assertEquals(nodeMap.get(4).getStrahlerNumber(), 3);
+        assertEquals(nodeMap.get(5).getStrahlerNumber(), 2);
+        assertEquals(nodeMap.get(6).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(7).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(8).getStrahlerNumber(), 2);
+        assertEquals(nodeMap.get(9).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(10).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(11).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(12).getStrahlerNumber(), 2);
+        assertEquals(nodeMap.get(13).getStrahlerNumber(), 2);
+        assertEquals(nodeMap.get(14).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(15).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(16).getStrahlerNumber(), 2);
+        assertEquals(nodeMap.get(17).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(18).getStrahlerNumber(), 2);
+        assertEquals(nodeMap.get(19).getStrahlerNumber(), 1);
+        assertEquals(nodeMap.get(20).getStrahlerNumber(), 1);
+    }
+
+    /**
+     * Initializes the node info map.
+     *
+     * @param graph The input graph.
+     *
+     * @return The newly initialized node info map.
+     */
+    protected Map<Integer, StrahlerInfo> init(Graph graph) {
+        Map<Integer, StrahlerInfo> nodeMap =
+                new HashMap<Integer, StrahlerInfo>();
+        TIntHashSet nodeSet = GeneralizedGraphAnalyzer.nodeSet(graph);
+        TIntIterator it = nodeSet.iterator();
+        while (it.hasNext()) {
+            nodeMap.put(it.next(), new StrahlerInfo());
+        }
+        return nodeMap;
+    }
+}
