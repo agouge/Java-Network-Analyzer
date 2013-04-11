6bbc57a8aff4c1d0dbce444b79167d7e31259730
Adam Gouge <adam.gouge@ec-nantes.fr>
2013-04-11 16:49:26 +0200

    Refactor tests into alg package.

--- src/test/java/com/graphhopper/sna/alg/StrahlerTest.java
+++ src/test/java/com/graphhopper/sna/alg/StrahlerTest.java
@@ -22,7 +22,7 @@
  * You should have received a copy of the GNU General Public License along with
  * GraphHopper-SNA. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.graphhopper.sna.analyzers;
+package com.graphhopper.sna.alg;
 
 import com.graphhopper.sna.data.StrahlerInfo;
 import com.graphhopper.storage.Graph;
