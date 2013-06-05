359c2b8869b0583406c84975e38e9d25348c9b7e
Adam Gouge <adam.gouge@ec-nantes.fr>
2013-06-05 15:43:50 +0200

    Add AccessibilityAnalyzer.

---
+++ src/main/java/org/javanetworkanalyzer/analyzers/AccessibilityAnalyzer.java
@@ -0,0 +1,84 @@
+/**
+ * Java Network Analyzer provides a collection of graph theory and social
+ * network analysis algorithms implemented on mathematical graphs using the
+ * <a href="http://www.jgrapht.org/">JGraphT</a> library.
+ *
+ * Java Network Analyzer is distributed under the GPL 3 license. It is produced
+ * by the "Atelier SIG" team of the <a href="http://www.irstv.fr">IRSTV
+ * Institute</a>, CNRS FR 2488.
+ *
+ * Copyright 2013 IRSTV (CNRS FR 2488).
+ *
+ * Java Network Analyzer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Java Network Analyzer is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Java Network Analyzer. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.javanetworkanalyzer.analyzers;
+
+import java.util.Set;
+import org.javanetworkanalyzer.alg.DijkstraForAccessibility;
+import org.javanetworkanalyzer.data.VAccess;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.EdgeReversedGraph;
+
+/**
+ * Calculates, for each vertex, the (distance to the) closest destination among
+ * several possible destinations.
+ *
+ * @author Adam Gouge
+ */
+public class AccessibilityAnalyzer<E> extends GeneralizedGraphAnalyzer<VAccess, E> {
+
+    /**
+     * The set of destinations.
+     */
+    private Set<VAccess> destinations;
+
+    /**
+     * Constructor: sets the graph.
+     *
+     * @param graph Graph
+     */
+    public AccessibilityAnalyzer(Graph<VAccess, E> graph,
+                                 Set<VAccess> destinations) {
+        super(graph);
+        this.destinations = destinations;
+    }
+
+    /**
+     * Performs accessibility analysis.
+     */
+    public void compute() {
+        // If the graph is directed, then reverse it.
+        Graph<VAccess, E> g;
+        if (graph instanceof DirectedGraph) {
+            g = new EdgeReversedGraph<VAccess, E>((DirectedGraph) graph);
+        } else {
+            g = graph;
+        }
+        // Obtain a Dijkstra algorithm on the reversed graph.
+        DijkstraForAccessibility<E> dijkstra =
+                new DijkstraForAccessibility<E>(g);
+        // Now shortest paths from each destination the reversed graph
+        // correspond to shortest paths to each destination in the original
+        // graph.
+        for (VAccess dest : destinations) {
+            // Update the distance of each destination to itself.
+            dest.setClosestDestinationId(dest.getID());
+            dest.setDistanceToClosestDestination(0.0);
+            // Calculate all shortest paths from this destination and update
+            // the closest destination accordingly.
+            dijkstra.calculate(dest);
+        }
+    }
+}
---
+++ src/test/java/org/javanetworkanalyzer/analyzers/AccessibilityAnalyzerTest.java
@@ -0,0 +1,196 @@
+/**
+ * Java Network Analyzer provides a collection of graph theory and social
+ * network analysis algorithms implemented on mathematical graphs using the
+ * <a href="http://www.jgrapht.org/">JGraphT</a> library.
+ *
+ * Java Network Analyzer is distributed under the GPL 3 license. It is produced
+ * by the "Atelier SIG" team of the <a href="http://www.irstv.fr">IRSTV
+ * Institute</a>, CNRS FR 2488.
+ *
+ * Copyright 2013 IRSTV (CNRS FR 2488).
+ *
+ * Java Network Analyzer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Java Network Analyzer is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Java Network Analyzer. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.javanetworkanalyzer.analyzers;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.javanetworkanalyzer.data.VAccess;
+import org.javanetworkanalyzer.graphcreators.CormenGraphPrep;
+import org.javanetworkanalyzer.model.DirectedG;
+import org.javanetworkanalyzer.model.DirectedWeightedPseudoG;
+import org.javanetworkanalyzer.model.Edge;
+import org.javanetworkanalyzer.model.KeyedGraph;
+import org.javanetworkanalyzer.model.UndirectedG;
+import org.javanetworkanalyzer.model.WeightedEdgeReversedG;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests the {@link AccessibilityAnalyzer} on the Cormen graph under all
+ * possible configurations.
+ *
+ * @author Adam Gouge
+ */
+public class AccessibilityAnalyzerTest extends CormenGraphPrep {
+
+    private static final double TOLERANCE = 0.0;
+
+    @Test
+    public void testD() throws Exception {
+        DirectedG<VAccess, Edge> g = directed();
+        test(g);
+        assertEquals(4, g.getVertex(1).getClosestDestinationId());
+        assertEquals(1.0, g.getVertex(1).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(2).getClosestDestinationId());
+        assertEquals(1.0, g.getVertex(2).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(5, g.getVertex(3).getClosestDestinationId());
+        assertEquals(1.0, g.getVertex(3).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(4).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(4).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(5, g.getVertex(5).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(5).getDistanceToClosestDestination(),
+                     TOLERANCE);
+    }
+
+    @Test
+    public void testR() throws Exception {
+        DirectedG<VAccess, Edge> g = reversed();
+        test(g);
+        assertEquals(5, g.getVertex(1).getClosestDestinationId());
+        assertEquals(1.0, g.getVertex(1).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(2).getClosestDestinationId());
+        assertEquals(1.0, g.getVertex(2).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        // The closest destination could be either 4 or 5.
+        if (g.getVertex(3).getClosestDestinationId() != 4
+            && g.getVertex(3).getClosestDestinationId() != 5) {
+            assertTrue(false);
+        }
+        assertEquals(1.0, g.getVertex(3).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(4).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(4).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(5, g.getVertex(5).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(5).getDistanceToClosestDestination(),
+                     TOLERANCE);
+    }
+
+    @Test
+    public void testU() throws Exception {
+        UndirectedG<VAccess, Edge> g = undirected();
+        test(g);
+        // The closest destination could be either 4 or 5.
+        if (g.getVertex(1).getClosestDestinationId() != 4
+            && g.getVertex(1).getClosestDestinationId() != 5) {
+            assertTrue(false);
+        }
+        assertEquals(1.0, g.getVertex(1).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(2).getClosestDestinationId());
+        assertEquals(1.0, g.getVertex(2).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        // The closest destination could be either 4 or 5.
+        if (g.getVertex(3).getClosestDestinationId() != 4
+            && g.getVertex(3).getClosestDestinationId() != 5) {
+            assertTrue(false);
+        }
+        assertEquals(1.0, g.getVertex(3).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(4).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(4).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(5, g.getVertex(5).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(5).getDistanceToClosestDestination(),
+                     TOLERANCE);
+    }
+
+    @Test
+    public void testWD() throws Exception {
+        DirectedWeightedPseudoG<VAccess, Edge> g = weightedDirected();
+        test(g);
+        assertEquals(4, g.getVertex(1).getClosestDestinationId());
+        assertEquals(5.0, g.getVertex(1).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(2).getClosestDestinationId());
+        assertEquals(2.0, g.getVertex(2).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(5, g.getVertex(3).getClosestDestinationId());
+        assertEquals(4.0, g.getVertex(3).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(4).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(4).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(5, g.getVertex(5).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(5).getDistanceToClosestDestination(),
+                     TOLERANCE);
+    }
+
+    @Test
+    public void testWR() throws Exception {
+        WeightedEdgeReversedG<VAccess, Edge> g = weightedReversed();
+        test(g);
+        assertEquals(5, g.getVertex(1).getClosestDestinationId());
+        assertEquals(7.0, g.getVertex(1).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(2).getClosestDestinationId());
+        assertEquals(3.0, g.getVertex(2).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(3).getClosestDestinationId());
+        assertEquals(4.0, g.getVertex(3).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(4).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(4).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(5, g.getVertex(5).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(5).getDistanceToClosestDestination(),
+                     TOLERANCE);
+    }
+
+    @Test
+    public void testWU() throws Exception {
+        UndirectedG<VAccess, Edge> g = weightedUndirected();
+        test(g);
+        assertEquals(4, g.getVertex(1).getClosestDestinationId());
+        assertEquals(5.0, g.getVertex(1).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(2).getClosestDestinationId());
+        assertEquals(2.0, g.getVertex(2).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(3).getClosestDestinationId());
+        assertEquals(3.0, g.getVertex(3).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(4, g.getVertex(4).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(4).getDistanceToClosestDestination(),
+                     TOLERANCE);
+        assertEquals(5, g.getVertex(5).getClosestDestinationId());
+        assertEquals(0.0, g.getVertex(5).getDistanceToClosestDestination(),
+                     TOLERANCE);
+    }
+
+    public void test(KeyedGraph<VAccess, Edge> g) throws Exception {
+        // Prepare the destinations.
+        Set<VAccess> destinations = new HashSet<VAccess>();
+        destinations.add(g.getVertex(5));
+        destinations.add(g.getVertex(4));
+        // Do accessibility analysis.
+        new AccessibilityAnalyzer<Edge>(g, destinations).compute();
+    }
+}
